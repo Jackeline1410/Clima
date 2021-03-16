30fb849f026e1bab702caa94e5e341ae12848ec2
Jackeline <ing.jacke@gmail.com>
2021-03-16 05:00:58 -0500

    Creando el formulario y el State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+
 
 function App() {
   return (
@@ -7,6 +9,19 @@ function App() {
       <Header 
         titulo='Clima React App'
       /> 
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
